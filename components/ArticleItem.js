3cc90cd0840d84104307bf13a4ad250ac2ac1fb6
ShaunZh <308826842@qq.com>
2018-12-19 00:26:48 +0800

    添加文章列表组件

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -1,52 +1,80 @@
 /*
  * @Author: Hexon
  * @Date: 2018-12-08 16:32:43
- * @LastEditTime: 2018-12-09 01:04:10
+ * @LastEditTime: 2018-12-19 00:13:42
  * @Description: 文章列表项
  */
+import Link from "next/link";
 
- const wrapStyle = {
-   width: 581x;
-   display: flex;
-   padding: 7px;
-   > img {
-     width: 175px;
-     height: 120px;
-   }
-   > div {
-     flexGrow: 2;
-   }
- }
-
- const rightStyle = {
-   display: flex;
-   flexDirection: column;
-   > div {
-     flexGrow: 2;
-     alignSlef: flexEnd;
-   }
- }
-
- const bottomStyle = {
-   display: flex;
-   justifyContent: spaceBetween;
- }
-
- const ArticleItem = (props) => (
-   <div style={wrapStyle}>
-     <img src={props.articleImg} alt="articleImg"/>
-     <div style={rightStyle}>
-       <h5>{props.title}</h5>
-       <h6>{props.subTitle}</h6>
-        <div style={bottomStyle}>
+const ArticleItem = props => (
+  <div className="wrap">
+    <img className="left" src={props.articleBkImg} alt="articleImg" />
+    <div className="right flex flex-direction-column">
+      <Link
+        as={`/article/${props.id}`}
+        key={props.id}
+        href={`/article?id=${props.id}`}
+      >
+        <a>
+          <h4>{props.title}</h4>
+        </a>
+      </Link>
+      <h5>{props.subTitle}</h5>
+      <div className="bottom fs-12 flex align-item-end justify-content-between">
+        <div className="item">
+          <img src="/static/icons/date.svg" alt="" />
           <span>{props.createTime}</span>
+        </div>
+        <div className="item">
+          <img src="/static/icons/view.svg" alt="" />
           <span>{props.viewCount}</span>
+        </div>
+        <div className="item">
+          <img src="/static/icons/message.svg" alt="" />
           <span>{props.commentCount}</span>
+        </div>
+        <div className="item">
+          <img src="/static/icons/like.svg" alt="" />
           <span>{props.likeCount}</span>
+        </div>
+        <div className="item">
+          <img src="/static/icons/category.svg" alt="" />
           <span>{props.category}</span>
         </div>
-     </div>
-   </div>
- )
+      </div>
+    </div>
+    <style jsx>
+      {`
+        .wrap {
+          margin-bottom: 7px;
+          padding: 7px;
+          display: flex;
+          background-color: pink;
+        }
+        .left {
+          width: 175px;
+          height: 120px;
+        }
+        .right {
+          margin-left: 10px;
+          flex-grow: 2;
+          display: flex;
+          flex-direction: column;
+        }
+        .right > .bottom {
+          flex-grow: 2;
+        }
 
- export default ArticleItem;
+        .right > .bottom > .item {
+          margin-left: 7px;
+        }
+        .right > .bottom img {
+          margin-right: 4px;
+          vertical-align: middle;
+        }
+      `}
+    </style>
+  </div>
+);
+
+export default ArticleItem;
--- components/ArticlesList.js
+++ components/ArticlesList.js
@@ -1,18 +1,23 @@
 /*
  * @Author: Hexon
  * @Date: 2018-12-08 16:32:31
- * @LastEditTime: 2018-12-08 16:51:53
+ * @LastEditTime: 2018-12-19 00:14:29
  * @Description: 文章列表
  */
-import ArticleItem from "ArticleItem";
+import ArticleItem from "./ArticleItem";
 
-const ArticlesList = pros => (
+const ArticlesList = ({articlesList})=> (
   <ul>
-    {props.articlesList.map(item => (
+    {articlesList.map(item => (
       <li key={item.articleId}>
         <ArticleItem {...item} />
       </li>
     ))}
+    <style jsx>{`
+      ul, li {
+        list-style: none;
+      }
+      `}</style>
   </ul>
 );
 
