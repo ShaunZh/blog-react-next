74c9e4c9588e2e6cc3ff202a40d7f68655eac569
ShaunZh <308826842@qq.com>
2018-12-19 00:32:21 +0800

    添加search组件

---
+++ components/Search.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: Hexon
+ * @Date: 2018-12-08 16:58:04
+ * @LastEditTime: 2018-12-13 00:46:55
+ * @Description: 搜索栏
+ */
+
+ const Search = (props) => (
+   <div className="search-wrap">
+    <input type="text"/>
+    <style jsx>{`
+      .search-wrap {
+        padding: 7px 15px;
+        background: #f8f8f8;
+      }
+      `}
+    </style>
+   </div>
+ )
+
+ export default Search;
