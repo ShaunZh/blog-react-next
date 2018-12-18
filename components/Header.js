a402ce9d33cd1db894ca8db287e993f4ec7d8d44
ShaunZh <308826842@qq.com>
2018-12-19 00:28:03 +0800

    添加自定义Header

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,18 @@
 import Link from "next/link";
 
-const wrapStyle = {
-  height: 65px;
-  margin: 0 192px;
-  display: flex;
-  alignItems: center;
-  opacity: 0.8;
-};
 
 const Header = () => (
-  <div style={wrapStyle}>
+  <div className="header-wrap">
     <p>Hexon </p>
     <p>放风筝的小小马</p>
+    <style jsx>{`
+      div.header-wrap {
+        height: 65px;
+        padding: 0 200px;
+        display: flex;
+        align-items: center;
+      }
+    `}</style>
   </div>
 );
 
