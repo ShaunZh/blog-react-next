15b3a5c0e9a556307f34b0286fa2bf7ef1c5a0e4
ShaunZh <308826842@qq.com>
2018-12-19 00:29:07 +0800

    分类页面

--- pages/category.js
+++ pages/category.js
@@ -1,18 +1,33 @@
-// 分类页面
+/*
+ * @Author: Hexon
+ * @Date: 2018-12-04 21:51:42
+ * @LastEditTime: 2018-12-19 00:03:32
+ * @Description: 首页
+ */
+import Link from "next/link";
 import fetch from "isomorphic-unfetch";
-import Head from "next/head";
 
-export default const Category = () => (
-  <div className="category">
-    <h1>分类列表页面</h1>
-  </div>
-);
+import ArticlesList from "../components/ArticlesList";
+import "../global.scss";
 
-Category.getInitialProps = async function({ req }) {
-  // 获取分类列表数据
-  const res = await fetch(''); 
-  const data = await res.json();
-  return {
-    categoryList: data
+const Category = props => <ArticlesList articlesList={props.articlesList} />;
+
+Category.getInitialProps = async function(props) {
+  try {
+    const categoryId = props.query.id;
+    const res = await fetch(
+      `https://www.easy-mock.com/mock/5a6ad0ef2d33bf493f88235d/blog_copy/categorysList/${categoryId}`
+    );
+    const data = await res.json();
+    if (data.status !== 200) {
+      throw new Error(data.message);
+    }
+    return {
+      articlesList: data.data.articlesList
+    };
+  } catch (e) {
+    console.error(e.message);
   }
-}
+};
+
+export default Category;
