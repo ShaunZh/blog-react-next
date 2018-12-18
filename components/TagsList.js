ab3cea0e4846e642c458d63f67d8e5152551c4bf
ShaunZh <308826842@qq.com>
2018-12-19 00:28:43 +0800

    添加tagsList组件

--- components/TagsList.js
+++ components/TagsList.js
@@ -1,28 +1,50 @@
 /*
  * @Author: Hexon
  * @Date: 2018-12-08 16:57:13
- * @LastEditTime: 2018-12-09 00:40:41
+ * @LastEditTime: 2018-12-19 00:07:21
  * @Description: 标签列表
  */
 
- const wrapStyle = {
-   display: flex;
-   flexWrap: wrap;
-   marginLeft: 10px;
- }
+import Link from "next/link";
 
- const TagsList = (props) => (
-   <ul style={wrapStyle}>
-     {props.tagsList.map(item => (
-       <li>
-         <img src={item.tagIcon} alt="tagIcon"/>
-         <span>{item.tagName}</span>
-         <span>({item.articlesCount})</span>
-       </li>
-     ))}
-   </ul>
- )
+const TagsList = ({ tagsList = [] }) => (
+  <nav className="nav-list">
+    {tagsList.map(item => (
+      <Link
+        as={`/tag/${item.tagName}`}
+        key={item.tagId}
+        href={`/tag?id=${item.tagId}`}
+      >
+        <a className="nav-item text-inline">
+          <img className="tag-icon" src={item.tagIcon} alt="tagIcon" className="tag-icon" />
+          <span>{item.tagName}</span>
+          <span>({item.articlesCount})</span>
+        </a>
+      </Link>
+    ))}
+    <style jsx>
+      {`
+        .nav-list {
+          display: flex;
+          flex-wrap: wrap;
+          align-items: center;
+          padding: 12px;
+          background: #f8f8f8;
+        }
+        .nav-item {
+          background: #dfdfdf;
+          margin-bottom: 12px;
+          padding: 7px;
+        }
+        .nav-item > .tag-icon {
+          width: 24px;
+          height: 24px;
+          margin-right: 6px;
+          vertical-align: middle;
+        }
+      `}
+    </style>
+  </nav>
+);
 
- 
-
- export default TagsList;
+export default TagsList;
